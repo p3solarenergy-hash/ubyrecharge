99b01f64fd7f6120037b4c8d9c9beb59dd02e6c1
Eduardo <seu-email@gmail.com>
2026-05-16 20:48:33 -0300

    Enable Supabase project connection

--- docs/obra-ev/supabase_config.js
+++ docs/obra-ev/supabase_config.js
@@ -1,5 +1,5 @@
 window.UBY_SUPABASE_CONFIG = {
-  url: "",
-  anonKey: "",
-  enabled: false
+  url: "https://csxafzuaqbbsbdatuhrd.supabase.co",
+  anonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNzeGFmenVhcWJic2JkYXR1aHJkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg5Njg5ODMsImV4cCI6MjA5NDU0NDk4M30.Kb_4MIW4unNjFFLSzUDr-MDpEQmGOZbIyQTzCCZArcs",
+  enabled: true
 };
